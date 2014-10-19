fed82001a93d2ee4915895b782b06debc755685b
Elías Grande Cásedas <elias.grande.casedas@gmail.com>
2014-10-19 21:02:41 +0200

    Add icon

--- dist/releases/latest.meta.js
+++ dist/releases/latest.meta.js
@@ -4,6 +4,7 @@
 // @namespace    https://github.com/EliasGrande/
 // @downloadURL  https://github.com/EliasGrande/OGameTradeCalculator/raw/master/dist/releases/latest.user.js
 // @updateURL    https://github.com/EliasGrande/OGameTradeCalculator/raw/master/dist/releases/latest.meta.js
+// @icon         https://github.com/EliasGrande/OGameTradeCalculator/raw/master/dist/img/icon.png
 // @version      3.0.0
 // @include      *://*.ogame.*/game/index.php?*page=*
 // ==/UserScript==
